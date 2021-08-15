8454c6c84ee685ca288d7d9b2ffa9caa7d1d908d
Stradh <60433907+Stradh@users.noreply.github.com>
2021-08-15 01:13:10 -0400

    Main Menu

---
+++ Assets/Content/Scenes/Main Menu/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] List<Button> mainMenuButtons = new List<Button>();
+
+    private int buttonIndex = -1;
+
+    private void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(mainMenuButtons[0].gameObject);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
