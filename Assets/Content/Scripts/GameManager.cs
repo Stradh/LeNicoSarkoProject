53149af3fa6633936afc34b612bf34d9e007468f
Stradh <60433907+Stradh@users.noreply.github.com>
2021-08-18 23:47:43 -0400

    First Dialog

---
+++ Assets/Content/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        DialogueManager.StartConversation("projet 001");
+    }
+
+    void Update()
+    {
+        
+    }
+}
